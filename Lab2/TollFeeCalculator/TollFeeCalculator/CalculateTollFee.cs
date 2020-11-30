15a155786f155e93520d33a38b461f1591c14012
Khosro Jadidi <khosro.jadidi@consultant.pulsen.se>
2020-11-30 14:34:57 +0100

    added some new tests

--- Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculator/CalculateTollFee.cs
@@ -2,24 +2,24 @@
 
 namespace TollFeeCalculator
 {
-    class Program
+    public class Program
     {
         static void Main()
         {
             run(Environment.CurrentDirectory + "../../../../testData.txt");
         }
 
-        static void run(String inputFile) {
+        public static void run(String inputFile) {
             string indata = System.IO.File.ReadAllText(inputFile);
             String[] dateStrings = indata.Split(", ");
             DateTime[] dates = new DateTime[dateStrings.Length-1];
             for(int i = 0; i < dates.Length; i++) {
                 dates[i] = DateTime.Parse(dateStrings[i]);
             }
-            Console.Write("The total fee for the inputfile is" + TotalFeeCost(dates));
+            Console.Write("The total fee for the inputfile is " + TotalFeeCost(dates));
         }
 
-        static int TotalFeeCost(DateTime[] d) {
+        public static int TotalFeeCost(DateTime[] d) {
             int fee = 0;
             DateTime si = d[0]; //Starting interval
             foreach (var d2 in d)
@@ -35,7 +35,7 @@ namespace TollFeeCalculator
             return Math.Max(fee, 60);
         }
 
-        static int TollFeePass(DateTime d)
+        public static int TollFeePass(DateTime d)
         {
             if (free(d)) return 0;
             int hour = d.Hour;
@@ -51,8 +51,9 @@ namespace TollFeeCalculator
             else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
             else return 0;
         }
+
         //Gets free dates
-        static bool free(DateTime day) {
+        public static bool free(DateTime day) {
         return (int)day.DayOfWeek == 5 || (int)day.DayOfWeek == 6 || day.Month == 7;
         }
     }
--- Lab2/TollFeeCalculator/TollFeeCalculatorTests/UnitTest1.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculatorTests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using TollFeeCalculator;
 
 namespace TollFeeCalculatorTests
 {
@@ -6,8 +9,46 @@ namespace TollFeeCalculatorTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RunTest()
         {
+            using(var stringWriter= new StringWriter())
+            {
+                Console.SetOut(stringWriter);                
+                Program.run(
+                    Environment.CurrentDirectory +
+                    "../../../../../TollFeeCalculator/testData.txt"
+                );
+                var expected  = "The total fee for the inputfile is 55";
+                Assert.AreEqual(expected, stringWriter.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void TotalFeeCostTest()
+        {
+            var passingDates = new DateTime[] 
+            {
+            new DateTime(2020,6,30,00,5,00),
+            new DateTime(2020,6,30,06,34,00),
+            new DateTime(2020,6,30,08,52,00),
+            new DateTime(2020,6,30,10,13,00),
+            new DateTime(2020,6,30,10,25,00),
+            new DateTime(2020,6,30,11,4,00),
+            new DateTime(2020,6,30,16,50,00),
+            new DateTime(2020,6,30,18,0,00),
+            new DateTime(2020,6,30,21,30,00),
+            new DateTime(2020,7,01,00,0,00) 
+            };
+            var actual = Program.TotalFeeCost(passingDates);
+            var expected = 55;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TotalFeeCostTest()
+        {
+            
+            Assert.AreEqual(expected, actual);
         }
     }
 }
