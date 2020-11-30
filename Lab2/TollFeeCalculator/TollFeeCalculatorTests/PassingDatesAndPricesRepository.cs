5624b8cf8891c4c1cbd9a93caef29ecd607f0a0d
Khosro Jadidi <khosro.jadidi@consultant.pulsen.se>
2020-11-30 15:17:43 +0100

    added arrays for dates and prices

---
+++ Lab2/TollFeeCalculator/TollFeeCalculatorTests/PassingDatesAndPricesRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculatorTests
+{
+    public static class PassingDatesAndPricesRepository
+    {
+        internal static readonly DateTime[] dates = new[]
+        {
+            new DateTime(2020,6,30,00,50,0),
+            new DateTime(2020,6,30,06,34,00),
+            new DateTime(2020,6,30,08,52,00),
+            new DateTime(2020,6,30,10,13,00),
+            new DateTime(2020,6,30,10,25,00),
+            new DateTime(2020,6,30,11,4,00),
+            new DateTime(2020,6,30,16,50,00),
+            new DateTime(2020,6,30,18,0,00),
+            new DateTime(2020,6,30,21,30,00),
+            new DateTime(2020,7,01,00,0,00)
+        };
+
+        internal static readonly Tuple<DateTime, int>[] datesAndPrices = new[]
+        {
+            new Tuple<DateTime,int>(dates[0], 0),
+            new Tuple<DateTime,int>(dates[1], 13),
+            new Tuple<DateTime,int>(dates[2], 8),
+            new Tuple<DateTime,int>(dates[3], 8),
+            new Tuple<DateTime,int>(dates[4], 8),
+            new Tuple<DateTime,int>(dates[5], 8),
+            new Tuple<DateTime,int>(dates[6], 18),
+            new Tuple<DateTime,int>(dates[7], 8),
+            new Tuple<DateTime,int>(dates[8], 0),
+            new Tuple<DateTime,int>(dates[9], 0)
+        };
+    }
+}
--- Lab2/TollFeeCalculator/TollFeeCalculatorTests/UnitTest1.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculatorTests/UnitTest1.cs
@@ -8,6 +8,34 @@ namespace TollFeeCalculatorTests
     [TestClass]
     public class UnitTest1
     {
+        private static readonly DateTime[] dates = new[]
+        {
+            new DateTime(2020,6,30,00,50,0),
+            new DateTime(2020,6,30,06,34,00),
+            new DateTime(2020,6,30,08,52,00),
+            new DateTime(2020,6,30,10,13,00),
+            new DateTime(2020,6,30,10,25,00),
+            new DateTime(2020,6,30,11,4,00),
+            new DateTime(2020,6,30,16,50,00),
+            new DateTime(2020,6,30,18,0,00),
+            new DateTime(2020,6,30,21,30,00),
+            new DateTime(2020,7,01,00,0,00)
+        };
+
+        private readonly Tuple<DateTime,int>[] datesAndPrices = new []
+        {
+            new Tuple<DateTime,int>(dates[0], 0),
+            new Tuple<DateTime,int>(dates[1], 13),
+            new Tuple<DateTime,int>(dates[2], 8),
+            new Tuple<DateTime,int>(dates[3], 8),
+            new Tuple<DateTime,int>(dates[4], 8),
+            new Tuple<DateTime,int>(dates[5], 8),
+            new Tuple<DateTime,int>(dates[6], 18),
+            new Tuple<DateTime,int>(dates[7], 8),
+            new Tuple<DateTime,int>(dates[8], 0),
+            new Tuple<DateTime,int>(dates[9], 0)
+        };
+
         [TestMethod]
         public void RunTest()
         {
@@ -26,29 +54,24 @@ namespace TollFeeCalculatorTests
         [TestMethod]
         public void TotalFeeCostTest()
         {
-            var passingDates = new DateTime[] 
-            {
-            new DateTime(2020,6,30,00,5,00),
-            new DateTime(2020,6,30,06,34,00),
-            new DateTime(2020,6,30,08,52,00),
-            new DateTime(2020,6,30,10,13,00),
-            new DateTime(2020,6,30,10,25,00),
-            new DateTime(2020,6,30,11,4,00),
-            new DateTime(2020,6,30,16,50,00),
-            new DateTime(2020,6,30,18,0,00),
-            new DateTime(2020,6,30,21,30,00),
-            new DateTime(2020,7,01,00,0,00) 
-            };
-            var actual = Program.TotalFeeCost(passingDates);
+            var dates = PassingDatesAndPricesRepository.dates;
+            var actual = Program.TotalFeeCost(dates);
             var expected = 55;
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TotalFeeCostTest()
+        public void TullFeePassTest()
         {
-            
-            Assert.AreEqual(expected, actual);
+
+            for (int i = 0; i < dates.Length; i++)
+            {
+                var actual = Program.TollFeePass(PassingDatesAndPricesRepository.datesAndPrices[i].Item1);
+                var expected = PassingDatesAndPricesRepository.datesAndPrices[i].Item2;
+
+                Assert.AreEqual(expected, actual);
+            }
+
         }
     }
 }
