eeb893a0e6366287198c6715b4b24778c306cf6d
AndersOrlinder <anders_orlinder@hotmail.com>
2020-12-02 02:22:35 +0100

    New isFreeDate test method

--- Lab2/TollFeeCalculator/TollFeeCalculatorTests/CalculateTollFeeTests.cs
+++ Lab2/TollFeeCalculator/TollFeeCalculatorTests/CalculateTollFeeTests.cs
@@ -28,14 +28,28 @@ namespace TollFeeCalculatorTests
         [TestMethod]
         public void TotalFeeCostTest()
         {
-            var dates = PassingDatesAndPrices.dates;
+            var dates = CalculateTollFeeTests.dates;
             var actual = CalculateTollFee.TotalFeeCost(dates);
             var expected = 55;
             Assert.AreEqual(expected, actual);
         }
 
+        internal static readonly DateTime[] dates = new[]
+        {
+            new DateTime(2020,6,30,00,50,0),
+            new DateTime(2020,6,30,06,34,00),
+            new DateTime(2020,6,30,08,52,00),
+            new DateTime(2020,6,30,10,13,00),
+            new DateTime(2020,6,30,10,25,00),
+            new DateTime(2020,6,30,11,4,00),
+            new DateTime(2020,6,30,16,50,00),
+            new DateTime(2020,6,30,18,0,00),
+            new DateTime(2020,6,30,21,30,00),
+            new DateTime(2020,7,01,00,0,00)
+        };
+
         [DataTestMethod]
-        [DynamicData(nameof(DatesAndPrices), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(GetDatesAndPrices), DynamicDataSourceType.Method)]
         public void TullFeePassTest(string dateString, int fee)
         {
             var date = DateTime.ParseExact(dateString, "yyyy,MM,dd,HH,mm", CultureInfo.InvariantCulture);
@@ -45,7 +59,7 @@ namespace TollFeeCalculatorTests
             Assert.AreEqual(expected, actual);
         }
 
-        internal static IEnumerable<object[]> DatesAndPrices()
+        internal static IEnumerable<object[]> GetDatesAndPrices()
         {
             yield return new object[] { "2020,06,30,00,50", 0 };
             yield return new object[] { "2020,06,30,06,34", 13 };
@@ -57,6 +71,26 @@ namespace TollFeeCalculatorTests
             yield return new object[] { "2020,06,30,18,00", 8 };
             yield return new object[] { "2020,06,30,21,30", 0 };
             yield return new object[] { "2020,07,01,00,00", 0 };
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetFreePassDates), DynamicDataSourceType.Method)]
+        public void FreeTest(string dateString, bool isFreeDate)
+        {
+            var date = DateTime.ParseExact(dateString, "yyyy,MM,dd,HH,mm", CultureInfo.InvariantCulture);
+            var expected = isFreeDate;
+            var actual = CalculateTollFee.free(date);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        internal static IEnumerable<object[]> GetFreePassDates()
+        {
+            yield return new object[] { "2020,07,06,16,10", true };
+            yield return new object[] { "2020,12,02,10,13", false }; 
+            yield return new object[] { "2020,12,18,08,34", false }; // Friday is fee weekday
+            yield return new object[] { "2020,12,20,08,34", true }; // Sunday is fee free weekday
+            yield return new object[] { "2020,12,25,13,52", true }; // Christmas day is always a Sunday
         }
     }
 }
